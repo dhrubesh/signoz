a76b092d104f0fd2496eacede8255c2f24ef7a9a
dhrubesh-makeen <dhrubesh@makeen.io>
2021-04-18 17:04:53 +0530

    test commit

--- frontend/src/constants/app.ts
+++ frontend/src/constants/app.ts
@@ -2,4 +2,4 @@ export const WITHOUT_SESSION_PATH = ["/redirect"];
 
 export const AUTH0_REDIRECT_PATH = "/redirect";
 
-export const DEFAULT_AUTH0_APP_REDIRECTION_PATH = "/application";
+export const DEFAULT_AUTH0_APP_REDIRECTION_PATH = "/application ";
